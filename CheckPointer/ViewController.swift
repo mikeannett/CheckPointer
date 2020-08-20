134d8d09ac43714daa2ecc6d32599aeb64a75023
Mike Annett <mike.annett2@gmail.com>
2020-08-20 16:43:29 +0100

    Initial Commit

---
+++ CheckPointer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckPointer
+//
+//  Created by Mike on 20/08/2020.
+//  Copyright © 2020 mannett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
